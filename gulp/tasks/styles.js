fae04081f8aae63368ffa139e4d95e855e535b12
kidvan777 <ivankidyaev.ik@gmail.com>
2017-11-29 17:59:32 +0300

    up

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,13 +4,14 @@ var gulp = require('gulp'),
  	autoprefixer = require('autoprefixer'),
  	cssvars = require('postcss-simple-vars'),
  	cssImport = require('postcss-import'),
- 	mixins = require('postcss-mixins');
+ 	mixins = require('postcss-mixins'),
+ 	hexrgba = require('postcss-hexrgba'),
  	cssnext = require('cssnext'), 	
  	precss = require('precss');
 
 	gulp.task('styles',function(){
 		return gulp.src('./app/assets/styles/styles.css')
-			.pipe(postcss([cssImport,mixins,cssvars,nested,autoprefixer({browsers: ['last 6 version']}),cssnext,precss]))
+			.pipe(postcss([cssImport,mixins,cssvars,nested,hexrgba,autoprefixer({browsers: ['last 6 version']}),cssnext,precss]))
 			.on('error', function(errorInfo){
 				console.log(errorInfo.toString());
 				this.emit('end');
